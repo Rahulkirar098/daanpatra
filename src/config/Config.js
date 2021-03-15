00f8095759eb7f4d415798133a5736dfa32579dd
Rahulkirar098 <kirarrahul1996@gmail.com>
2021-03-16 00:37:50 +0530

    first commit

---
+++ src/config/Config.js
@@ -0,0 +1,1 @@
+export const apiUrl = "http://13.126.128.217"; //for DEVELOPMENT
---
+++ src/dashbord/Pages/Certificate.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./certificate.css";
+import Userprofile from "../../assect/avatar.svg";
+
+
+function Certificate() {
+
+
+
+    return (
+
+        <div className="certificate">
+
+            <h1 className="certificate-h1">Certificate</h1>
+            <br />
+            <br />
+            <img src={Userprofile} className="userprofile" alt="profile" />
+            <br />
+            <br />
+                <input type="file" onChange={(profile)=>{
+                    console.log(profile.target.files,"result aaya hai")
+                }} />
+                <br />
+                <br />
+                <h1 className="certificate-h1">Rahul kirar</h1>
+                 
+                <br />
+                <br />
+                <button type="submit">Submit</button>
+        </div>
+    )
+}
+
+export default Certificate
